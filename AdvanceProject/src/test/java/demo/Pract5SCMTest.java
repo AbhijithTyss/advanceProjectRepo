6ad3d1d79b1aa3a27eac1f6304d28a59908db5ca
Divya <Divya@DESKTOP-0Q9HAE4>
2023-10-19 17:33:42 +0530

    Pract5SCMTest added

--- AdvanceProject/src/test/java/demo/Pract5SCMTest.java
+++ AdvanceProject/src/test/java/demo/Pract5SCMTest.java
@@ -3,10 +3,10 @@ package demo;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Pract5SCM {
+public class Pract5SCMTest {
 
 	@Test
 	public void checkOnPoleSCM(){
-		Reporter.log("Check on pole scm");
+		Reporter.log("Check on pole scm",true);
 	}
 }
